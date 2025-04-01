bedecdb1dcbd281f6b20148dc0e21ed52f65776e
Leslie Williamson <leslie.williamson@live.ca>
2025-04-01 18:27:38 -0400

    Add model lookup functions for company name and stock price based on ticker symbol.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,11 @@
 class Stock < ApplicationRecord
+  def self.lookup_price(ticker_symbol)
+    quote = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
+    quote.price if quote
+  end
+
+  def self.lookup_company_name(ticker_symbol)
+    company = Alphavantage::Fundamental.new(symbol: ticker_symbol)
+    company.overview.name if company
+  end
 end
