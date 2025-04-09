c56c7182a61bde8c0a64dc3e5c78141634e4a805
Leslie Williamson <leslie.williamson@live.ca>
2025-04-09 15:27:49 -0400

    Add error and exception handling to ticker symbol lookup

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,16 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.lookup(params[:stock].upcase)
-    render "users/my_portfolio"
+    if  params[:stock].present?
+      @stock = Stock.lookup(params[:stock].upcase)
+      if @stock
+        render "users/my_portfolio"
+      else
+        flash.now[:alert] = "Please enter a valid ticker symbol to search"
+        render "users/my_portfolio"
+      end
+    else
+        #   flash.now[:alert] = "Please enter a ticker symbol to search"
+        render "users/my_portfolio"
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,12 @@
 class Stock < ApplicationRecord
   def self.lookup(ticker_symbol)
     # debugger
-    stock = Alphavantage::TimeSeries.new(symbol: ticker_symbol)
-    company = Alphavantage::Fundamental.new(symbol: ticker_symbol)
-    new(ticker: ticker_symbol, name: company.overview.name, last_price: stock.quote.price)
+    begin
+      stock = Alphavantage::TimeSeries.new(symbol: ticker_symbol)
+      company = Alphavantage::Fundamental.new(symbol: ticker_symbol)
+      new(ticker: ticker_symbol, name: company.overview.name, last_price: stock.quote.price)
+    rescue => exception
+
+    end
   end
 end
